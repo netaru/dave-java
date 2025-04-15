33b07b88e320d5b4fed61b31016e7e68f00f404c
David Jonsson <david.jonsson306@gmail.com>
2025-04-15 10:34:40 +0200

    First version of the package

--- dave-java.el
+++ dave-java.el
@@ -22,7 +22,11 @@
 
 ;;; Commentary:
 
-;; Fill here
+;; Some custom variables to ease the configuration of lsp-java. Convenience
+;; function to download lombok etc.
+;; 
+;; Improvements:
+;;  - Include downloading of a formatter
 
 ;;; Code:
 (defcustom dave-java-enable-lombok t
@@ -33,39 +37,52 @@
 (defcustom dave-java-lombok-path (concat (getenv "HOME") "/.local/share/dave/libs/lombok.jar")
   "Path to lombok jar file"
   :group 'dave-java
-  :type 'string)
+  :type 'file)
 
 (defcustom dave-java-lombok-download-url "https://repo1.maven.org/maven2/org/projectlombok/lombok/1.18.38/lombok-1.18.38.jar"
-  "url to lombok file"
+  "Url to lombok file"
   :group 'dave-java
   :type 'string)
 
 (defcustom dave-java-java-home "/usr/lib/jvm/default"
-  "docstring"
+  "Path to java home for the jvm running jdtls."
   :group 'dave-java
-  :type 'string)
+  :type 'directory)
 
 (defcustom dave-java-java-path "/usr/lib/jvm/java-17-openjdk/bin/java"
   "Path of the java executable. Used for lsp and other development tools."
   :group 'dave-java
-  :type 'string)
+  :type '(file :must-match t))
 
-;; A function to format stdin xml and put the result in the same buffer
+(defcustom dave-java-format-program-path (concat (getenv "HOME") "/.local/bin/palantirw")
+  "Path of the format executable. Used for formatting java code."
+  :group 'dave-java
+  :type 'file)
+
+(defcustom dave-java-format-program-args '("--skip-reflowing-long-strings" "--palantir" "-")
+  "Arguments passed to the format program `dave-java-format-program-path'".
+  :group 'dave-java
+  :type 'list)
+
+;;;###autoload
 (defun dave-java-palantir-format-buffer ()
   (interactive)
-  (let((buffer (generate-new-buffer "*java-palantir-format-buffer-temp*")))
-    (call-process-region nil nil (concat (getenv "HOME") "/.local/bin/palantirw") nil `(,buffer, nil) nil "--skip-reflowing-long-strings" "--palantir" "-")
-    (replace-buffer-contents buffer)
-    (kill-buffer buffer)))
+  (if (or (file-exists-p dave-java-format-program-path)
+          (executable-find dave-java-format-program-path))
+      (let ((buffer (generate-new-buffer "*dave-java-format-buffer-temp*")))
+        (apply #'call-process-region nil nil dave-java-format-program-path nil `(,buffer, nil) nil dave-java-format-program-args)
+        (replace-buffer-contents buffer)
+        (kill-buffer buffer))))
 
+;;;###autoload
 (defun dave-java-initialise-class (filename)
   (interactive)
   (let* ((id "src/\\(main\\|test\\)/java")
          (where (string-match id filename))
          (suffix (substring filename (+ 1 (match-end 0)))))
-    (progn
-      `(,(substring filename 0 (- (string-width suffix))) . ,(replace-regexp-in-string (regexp-quote "/") "." (file-name-directory suffix) nil 'literal)))))
+    `(,(substring filename 0 (- (string-width suffix))) . ,(replace-regexp-in-string (regexp-quote "/") "." (file-name-directory suffix) nil 'literal))))
 
+;;;###autoload
 (defun dave-java-create-class ()
   (interactive)
   (let* ((class-data (dave-java-initialise-class buffer-file-name))
@@ -81,45 +98,13 @@
       (f-write-text (concat "package " package-name ";\n\npublic class " class-name " {\n\n}\n") 'utf-8 filename))
     (find-file filename)))
 
+;;;###autoload
 (defun dave-java-install-lombok ()
   (interactive)
   (unless (file-exists-p dave-java-lombok-path)
     (progn
       (make-directory (file-name-directory dave-java-lombok-path) t)
       (url-copy-file dave-java-lombok-download-url dave-java-lombok-path))))
-
-(use-package lsp-sonarlint-java
-  :ensure nil
-  :custom
-  (lsp-sonarlint-java-enabled t))
-
-(use-package lsp-java
-  :general
-  (general-nmap
-    :keymaps 'java-ts-mode-map
-    ",fn" 'dave-java-create-class
-    ",tb" 'lsp-jt-browser
-    ",tl" 'lsp-jt-lens-mode
-    ",tr" 'lsp-jt-report-open
-    ",pb" 'lsp-java-build-project
-    "=" 'dave-java-palantir-format-buffer)
-  (general-nmap
-    :keymaps 'lsp-jt-mode-map
-    "r" 'lsp-jt-run
-    "d" 'lsp-jt-debug)
-  :custom
-  (lsp-java-java-path dave-java-java-path)
-  (lsp-java-autobuild-enabled nil)
-  :config
-  (setenv "JAVA_HOME" dave-java-java-home)
-  (if (and dave-java-enable-lombok
-           (file-exists-p dave-java-lombok-path))
-      (add-to-list 'lsp-java-vmargs (concat "-javaagent:" dave-java-lombok-path) t)))
-
-(use-package dap-java
-  :ensure nil)
-
-;;;###autoload
 
 (provide 'dave-projectile-utils)
 ;;; dave-java.el ends here
